77ef7b7176c3cb09a6484b24cb9c5fea2935350b
DESKTOP-NVGPU6S\user <thankosein@gmail.com>
2018-07-14 18:03:11 +0800

    add admin project

---
+++ Com.EazyTender_Admin/Configurations/AppSettings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Com.BudgetMetal.Common.FoundationClasses;
+
+namespace Com.BudgetMetal.Services.GalleryAPI.Configurations
+{
+    public class AppSettings : BaseSettings
+    {
+        public int TotalRecordPerPage
+        {
+            get;
+            set;
+        }
+
+        public int FirstPage
+        {
+            get;
+            set;
+        }
+    }
+}
