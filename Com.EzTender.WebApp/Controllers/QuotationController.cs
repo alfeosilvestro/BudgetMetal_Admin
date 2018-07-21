721b8ed88271f81dafbc34312b2c1cbfdf5928c6
Galles <galles.cs@gmail.com>
2018-07-21 14:55:46 +0800

    add quotation page

---
+++ Com.EzTender.WebApp/Controllers/QuotationController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Com.EzTender.WebApp.Controllers
+{
+    public class QuotationController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
