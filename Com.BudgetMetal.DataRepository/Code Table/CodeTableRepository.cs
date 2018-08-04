8b09c51af99846c2c0d0987810a88c48744a5f85
DESKTOP-NVGPU6S\user <thankosein@gmail.com>
2018-08-04 17:48:09 +0800

    code table listing has to use eager loading during select removed place holder file updated gitignore to ignore logs folder

--- Com.BudgetMetal.DataRepository/Code Table/CodeTableRepository.cs
+++ Com.BudgetMetal.DataRepository/Code Table/CodeTableRepository.cs
@@ -27,12 +27,14 @@ namespace Com.BudgetMetal.DataRepository.Code_Table
                 //return await base.GetPage(keyword, page, totalRecords);
             }
 
-            var records = this.DbContext.CodeTable
-                .Include("CodeCategory")
+            var records = entities
+                .Include(ct => ct.CodeCategory)
                 .Where(e =>
-                  (keyword == string.Empty ||
-                  e.Name.Contains(keyword))
-            );
+                  (keyword == string.Empty || e.Name.Contains(keyword))
+                )
+                .OrderBy(e => new { e.Name, e.CreatedDate })
+                .Skip((totalRecords * page) - totalRecords)
+                .Take(totalRecords);
 
             var recordList = records
             .Select(r =>
@@ -43,9 +45,6 @@ namespace Com.BudgetMetal.DataRepository.Code_Table
                     CodeCategory_Id = r.CodeCategory_Id,
                     CodeCategory = r.CodeCategory
                 })
-            .OrderBy(e => new { e.Name, e.CreatedDate })
-            .Skip((totalRecords * page) - totalRecords)
-            .Take(totalRecords)
             .ToList();
 
             var count = await records.CountAsync();
