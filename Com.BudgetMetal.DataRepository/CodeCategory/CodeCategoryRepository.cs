5b8df08228a641fbf719744902f63547e2aa86cb
DESKTOP-NVGPU6S\user <thankosein@gmail.com>
2018-09-01 11:48:47 +0800

    Add override GetPage method for Searching.

--- Com.BudgetMetal.DataRepository/CodeCategory/CodeCategoryRepository.cs
+++ Com.BudgetMetal.DataRepository/CodeCategory/CodeCategoryRepository.cs
@@ -18,6 +18,58 @@ namespace Com.BudgetMetal.DataRepository.Code_Category
         {
 
         }
-        
+
+        public override async Task<PageResult<CodeCategory>> GetPage(string keyword, int page, int totalRecords = 10)
+        {
+            if (string.IsNullOrEmpty(keyword))
+            {
+                keyword = string.Empty;
+                //return await base.GetPage(keyword, page, totalRecords);
+            }
+
+            var records = entities
+               .Where(e =>
+                 (e.IsActive == true) &&
+                 (keyword == string.Empty || e.Name.Contains(keyword))
+               )
+               .OrderBy(e => new { e.Name, e.CreatedDate })
+               .Skip((totalRecords * page) - totalRecords)
+               .Take(totalRecords);
+
+
+
+            var recordList = records.ToList();
+
+            var count = entities.Where(e =>
+                 (e.IsActive == true) &&
+                 (keyword == string.Empty || e.Name.Contains(keyword)))
+                 .ToList().Count();
+            //await records.CountAsync();
+
+            var nextPage = 0;
+            var prePage = 0;
+            if (page > 1)
+            {
+                prePage = page - 1;
+            }
+
+            var totalPage = (count + totalRecords - 1) / totalRecords;
+            if (page < totalPage)
+            {
+                nextPage = page + 1;
+            }
+
+            var result = new PageResult<CodeCategory>()
+            {
+                Records = recordList,
+                TotalPage = totalPage,
+                CurrentPage = page,
+                PreviousPage = prePage,
+                NextPage = nextPage,
+                TotalRecords = count
+            };
+
+            return result;
+        }
     }
 }
