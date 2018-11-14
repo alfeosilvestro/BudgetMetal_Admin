76fdaad513c31007e188ba2eb2c4c84cf645f066
Galles <galles.cs@gmail.com>
2018-11-14 20:44:09 +0800

    File template upload back end

--- Com.EzTender.WebApp/Controllers/RfqController.cs
+++ Com.EzTender.WebApp/Controllers/RfqController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Com.BudgetMetal.Common;
 using Com.BudgetMetal.Services.Attachment;
@@ -12,15 +13,25 @@ using Com.BudgetMetal.Services.RFQ;
 using Com.BudgetMetal.Services.Roles;
 using Com.BudgetMetal.Services.ServiceTags;
 using Com.BudgetMetal.Services.Users;
+using Com.BudgetMetal.ViewModels;
 using Com.BudgetMetal.ViewModels.Attachment;
 using Com.BudgetMetal.ViewModels.DocumentActivity;
 using Com.BudgetMetal.ViewModels.DocumentUser;
 using Com.BudgetMetal.ViewModels.InvitedSupplier;
+using Com.BudgetMetal.ViewModels.Requirement;
 using Com.BudgetMetal.ViewModels.Rfq;
+using Com.BudgetMetal.ViewModels.RfqPenalty;
+using Com.BudgetMetal.ViewModels.RfqPriceSchedule;
 using Com.BudgetMetal.ViewModels.Role;
+using Com.BudgetMetal.ViewModels.Sla;
+using Com.BudgetMetal.ViewModels.TemplateModel;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
 
 namespace Com.GenericPlatform.WebApp.Controllers
 {
@@ -34,8 +45,9 @@ namespace Com.GenericPlatform.WebApp.Controllers
         private readonly IRoleService roleService;
         private readonly IAttachmentService attachmentService;
         private readonly IQuotationService quotationService;
+        private IHostingEnvironment _hostingEnvironment;
 
-        public RfqController(IIndustryService industryService, IServiceTagsService serviceTagsService, ICompanyService companyService, IRFQService rfqService, IUserService userService, IRoleService roleService, IAttachmentService attachmentService, IQuotationService quotationService)
+        public RfqController(IIndustryService industryService, IServiceTagsService serviceTagsService, ICompanyService companyService, IRFQService rfqService, IUserService userService, IRoleService roleService, IAttachmentService attachmentService, IQuotationService quotationService, IHostingEnvironment hostingEnvironment)
         {
             this.industryService = industryService;
             this.serviceTagsService = serviceTagsService;
@@ -45,6 +57,7 @@ namespace Com.GenericPlatform.WebApp.Controllers
             this.roleService = roleService;
             this.attachmentService = attachmentService;
             this.quotationService = quotationService;
+            this._hostingEnvironment = hostingEnvironment;
         }
 
         // GET: Rfq
@@ -110,10 +123,10 @@ namespace Com.GenericPlatform.WebApp.Controllers
 
                     bool isRfqApprover = false;
                     int tmpUserId = Convert.ToInt32(User_Id);
-                    if(result.Document.DocumentUserDisplay != null)
+                    if (result.Document.DocumentUserDisplay != null)
                     {
                         var tmpDocumentUser = result.Document.DocumentUserDisplay.Where(e => e.User_Id == tmpUserId).First();
-                        if(tmpDocumentUser.Roles.Contains("RFQ Approver"))
+                        if (tmpDocumentUser.Roles.Contains("RFQ Approver"))
                         {
                             isRfqApprover = true;
                         }
@@ -266,7 +279,7 @@ namespace Com.GenericPlatform.WebApp.Controllers
 
                 //return RedirectToAction("Index");
 
-                var result =await rfqService.UpdateRFQ(Rfq);
+                var result = await rfqService.UpdateRFQ(Rfq);
                 if (result.IsSuccess)
                 {
                     return RedirectToAction("Index");
@@ -293,7 +306,7 @@ namespace Com.GenericPlatform.WebApp.Controllers
             if (rfqService.CheckRFQLimit(Convert.ToInt32(HttpContext.Session.GetString("Company_Id"))) == false)
             {
                 TempData["ErrorMessage"] = "RFQ Limitation per week is exceed. please contact admin to upgrade your account.";
-               
+
                 return RedirectToAction("ErrorForUser", "Home");
             }
 
@@ -323,23 +336,25 @@ namespace Com.GenericPlatform.WebApp.Controllers
                 int i = 0;
                 foreach (var itemFile in Request.Form.Files)
                 {
-                    if (itemFile.Length > 0)
+                    if (itemFile.Name.ToLower() != "fUpload")
                     {
-                        var tmpFileNameArr = itemFile.FileName.ToString().Split("\\");
-                        string tmpFileName = tmpFileNameArr.Last();
-                        var att = new VmAttachmentItem
+                        if (itemFile.Length > 0)
                         {
-                            FileName = tmpFileName,
-                            FileSize = itemFile.Length,
-                            FileBinary = Convert.ToBase64String(ConvertFiletoBytes(itemFile)),
-                            Description = Request.Form["fileDescriptionRFQ[]"].ToArray()[i].ToString(),
-                            CreatedBy = Rfq.CreatedBy,
-                            UpdatedBy = Rfq.UpdatedBy
-                        };
-
-                        listAttachment.Add(att);
-
+                            var tmpFileNameArr = itemFile.FileName.ToString().Split("\\");
+                            string tmpFileName = tmpFileNameArr.Last();
+                            var att = new VmAttachmentItem
+                            {
+                                FileName = tmpFileName,
+                                FileSize = itemFile.Length,
+                                FileBinary = Convert.ToBase64String(ConvertFiletoBytes(itemFile)),
+                                Description = Request.Form["fileDescriptionRFQ[]"].ToArray()[i].ToString(),
+                                CreatedBy = Rfq.CreatedBy,
+                                UpdatedBy = Rfq.UpdatedBy
+                            };
+                            listAttachment.Add(att);
+                        }
                     }
+
                     i++;
                 }
 
@@ -394,7 +409,7 @@ namespace Com.GenericPlatform.WebApp.Controllers
                 }
                 else
                 {
-                    TempData["ErrorMessage"] ="Error while creating RFQ. Please contact system administrator.";
+                    TempData["ErrorMessage"] = "Error while creating RFQ. Please contact system administrator.";
                     return RedirectToAction("ErrorForUser", "Home");
                 }
             }
@@ -583,6 +598,234 @@ namespace Com.GenericPlatform.WebApp.Controllers
             var readStream = new MemoryStream(Convert.FromBase64String(fileByeArray));
             var mimeType = "application/zip";
             return File(readStream, mimeType, fileName);
+        }
+
+        public JsonResult UploadTemplate()
+        {
+            var resultTemplate = new VmTemplate();
+            resultTemplate.result = new VmGenericServiceResult();
+            resultTemplate.List_Requirement = new List<VmRequirementItem>();
+            resultTemplate.List_SLA = new List<VmSlaItem>();
+            resultTemplate.List_Panalty = new List<VmPenaltyItem>();
+            resultTemplate.List_Pricing = new List<VmRfqPriceScheduleItem>();
+            try
+            {
+                IFormFile file = Request.Form.Files[0];
+                string folderName = "Upload";
+                string webRootPath = _hostingEnvironment.WebRootPath;
+                string newPath = Path.Combine(webRootPath, folderName, DateTime.Now.ToString("ddMMyyyyHHmmss"));
+                StringBuilder sb = new StringBuilder();
+                if (!Directory.Exists(newPath))
+                {
+                    Directory.CreateDirectory(newPath);
+                }
+                if (file.Length > 0)
+                {
+                    string sFileExtension = Path.GetExtension(file.FileName).ToLower();
+
+                    int countSheet = 0;
+                    string fullPath = Path.Combine(newPath, file.FileName);
+                    using (var stream = new FileStream(fullPath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                        stream.Position = 0;
+                        if (sFileExtension == ".xls")
+                        {
+                            HSSFWorkbook hssfwb = new HSSFWorkbook(stream); //This will read the Excel 97-2000 formats  
+                            countSheet = hssfwb.NumberOfSheets;
+                            for (int i = 0; i < countSheet - 1; i++)
+                            {
+                                ISheet sheet;
+                                sheet = hssfwb.GetSheetAt(i);
+
+                                if (sheet.SheetName.ToString().ToLower() == "requirements")
+                                {
+                                    resultTemplate.List_Requirement = getRequirementsFromTemplate(sheet);
+                                }
+                                else if (sheet.SheetName.ToString().ToLower() == "sla")
+                                {
+                                    resultTemplate.List_SLA = getSLAFromTemplate(sheet);
+                                }
+                                else if (sheet.SheetName.ToString().ToLower() == "penalty")
+                                {
+                                    resultTemplate.List_Panalty = getPaneltyFromTemplate(sheet);
+                                }
+                                else if (sheet.SheetName.ToString().ToLower() == "pricing")
+                                {
+                                    resultTemplate.List_Pricing = getPricingFromTemplate(sheet);
+                                }
+                                else
+                                {
+                                    //nothing
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            XSSFWorkbook hssfwb = new XSSFWorkbook(stream); //This will read 2007 Excel format  
+                            countSheet = hssfwb.NumberOfSheets;
+                            for (int i = 0; i < countSheet - 1; i++)
+                            {
+                                ISheet sheet;
+                                sheet = hssfwb.GetSheetAt(i);
+
+                                if (sheet.SheetName.ToString().ToLower() == "requirements")
+                                {
+                                    resultTemplate.List_Requirement = getRequirementsFromTemplate(sheet);
+                                }
+                                else if (sheet.SheetName.ToString().ToLower() == "sla")
+                                {
+                                    resultTemplate.List_SLA = getSLAFromTemplate(sheet);
+                                }
+                                else if (sheet.SheetName.ToString().ToLower() == "penalty")
+                                {
+                                    resultTemplate.List_Panalty = getPaneltyFromTemplate(sheet);
+                                }
+                                else if (sheet.SheetName.ToString().ToLower() == "pricing")
+                                {
+                                    resultTemplate.List_Pricing = getPricingFromTemplate(sheet);
+                                }
+                                else
+                                {
+                                    //nothing
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultTemplate.result.IsSuccess = false;
+                resultTemplate.result.MessageToUser = "Error while uploading template";
+            }
+
+
+
+            return new JsonResult(resultTemplate, new JsonSerializerSettings()
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+        }
+
+        private List<VmRfqPriceScheduleItem> getPricingFromTemplate(ISheet sheet)
+        {
+            var result = new List<VmRfqPriceScheduleItem>();
+            IRow headerRow = sheet.GetRow(0); //Get Header Row
+            int cellCount = headerRow.LastCellNum;
+
+            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+            {
+
+                IRow row = sheet.GetRow(i);
+                if (row == null) continue;
+                if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                try
+                {
+                    var resultItem = new VmRfqPriceScheduleItem();
+                    resultItem.ItemName = row.GetCell(0).ToString();
+                    resultItem.ItemDescription = row.GetCell(1).ToString();
+                    resultItem.InternalRefrenceCode = row.GetCell(2).ToString();
+                    resultItem.QuantityRequired = row.GetCell(3).ToString();
+
+                    result.Add(resultItem);
+                }
+                catch
+                {
+                    //nothing
+                }
+                
+            }
+            return result;
+        }
+
+        private List<VmPenaltyItem> getPaneltyFromTemplate(ISheet sheet)
+        {
+            var result = new List<VmPenaltyItem>();
+            IRow headerRow = sheet.GetRow(0); //Get Header Row
+            int cellCount = headerRow.LastCellNum;
+
+            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+            {
+
+                IRow row = sheet.GetRow(i);
+                if (row == null) continue;
+                if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                try
+                {
+                    var resultItem = new VmPenaltyItem();
+                    resultItem.BreachOfServiceDefinition = row.GetCell(0).ToString();
+                    resultItem.Description = row.GetCell(1).ToString();
+                    resultItem.PenaltyAmount = row.GetCell(2).ToString();
+
+                    result.Add(resultItem);
+                }
+                catch
+                {
+                    //nothing
+                }
+
+            }
+            return result;
+        }
+
+        private List<VmSlaItem> getSLAFromTemplate(ISheet sheet)
+        {
+            var result = new List<VmSlaItem>();
+            IRow headerRow = sheet.GetRow(0); //Get Header Row
+            int cellCount = headerRow.LastCellNum;
+
+            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+            {
+
+                IRow row = sheet.GetRow(i);
+                if (row == null) continue;
+                if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                try
+                {
+                    var resultItem = new VmSlaItem();
+                    resultItem.Requirement = row.GetCell(0).ToString();
+                    resultItem.Description = row.GetCell(1).ToString();
+
+                    result.Add(resultItem);
+                }
+                catch
+                {
+                    //nothing
+                }
+
+            }
+            return result;
+        }
+
+        private List<VmRequirementItem> getRequirementsFromTemplate(ISheet sheet)
+        {
+            var result = new List<VmRequirementItem>();
+            IRow headerRow = sheet.GetRow(0); //Get Header Row
+            int cellCount = headerRow.LastCellNum;
+
+            for (int i = (sheet.FirstRowNum + 1); i <= sheet.LastRowNum; i++) //Read Excel File
+            {
+
+                IRow row = sheet.GetRow(i);
+                if (row == null) continue;
+                if (row.Cells.All(d => d.CellType == CellType.Blank)) continue;
+                try
+                {
+                    var resultItem = new VmRequirementItem();
+                    resultItem.ServiceName = row.GetCell(0).ToString();
+                    resultItem.Description = row.GetCell(1).ToString();
+
+                    result.Add(resultItem);
+                }
+                catch
+                {
+                    //nothing
+                }
+
+            }
+            return result;
         }
     }
 }
