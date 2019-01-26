b32577adeba8d2175b2b17e9bcc745bf9812e841
Alex_Development_Machine <alfeo.silvestro@live.com>
2019-01-26 13:13:57 +0800

    Facebook client and service initial check in

---
+++ Com.BudgetMetal.Services/Facebook/IFacebookService.cs
@@ -0,0 +1,14 @@
+﻿using Com.BudgetMetal.ViewModels.Facebook;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.BudgetMetal.Services.Facebook
+{
+    public interface IFacebookService
+    {
+        Task<Account> GetAccountAsync(string accessToken);
+        Task PostOnWallAsync(string accessToken, string message);
+    }
+}
