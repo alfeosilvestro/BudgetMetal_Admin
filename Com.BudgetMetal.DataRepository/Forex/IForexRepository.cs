dc2ce5923e9ea7813dce1f4948c0d268205a92d5
Alex_Development_Machine <alfeo.silvestro@live.com>
2019-02-23 12:31:57 +0800

    Forex Repository via Central Bank API

---
+++ Com.BudgetMetal.DataRepository/Forex/IForexRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.BudgetMetal.DataRepository.Forex
+{
+    public interface IForexRepository
+    {
+        Task<string> GetForexDataFromBankApi();
+    }
+}
